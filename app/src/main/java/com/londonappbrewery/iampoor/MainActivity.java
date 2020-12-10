be765060ec41859081aa6974364626df1b6a98f0
valen <valentin.bretel2020@campus-eni.fr>
2020-12-10 19:09:32 +0100

    Challenge complete

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -2,7 +2,8 @@ package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
